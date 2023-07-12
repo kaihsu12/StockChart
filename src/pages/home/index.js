3f06d79f0d91a6231d7c6ef69bda31e1f32d198a
OwenLu0125 <121356760+OwenLu0125@users.noreply.github.com>
2023-07-12 15:38:09 +0800

    Feat/routersetting (#2)
    * chore: route package install
    
    * feat: add routes function

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,14 +1,24 @@
+// react
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+// page
 import LoginPage from '../loginPage/loginPage';
 import SignupPage from '../signupPage/signupPage';
+// style
 import './reset.scss'
 import './base.scss'
 
 const Home = () => {
   return (
     <div>
-      <LoginPage />
-      {/* <SignupPage /> */}
+      <BrowserRouter>
+        <Routes>
+          <Route path='*' element={<LoginPage />} />
+          <Route path='signup' element={<SignupPage />} />
+          {/* <Route path='*' element={<HomePage />} /> */}
+        </Routes>
+      </BrowserRouter>
+
     </div>
   );
 };
