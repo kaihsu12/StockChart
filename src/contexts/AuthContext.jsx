2d90957e918588c3578bc4bb0eafc57cf3775012
OwenLu <gg0125gg@gmail.com>
2023-07-15 16:40:43 +0800

    feat: add register context

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -1,11 +1,14 @@
+// react
 import { createContext, useState } from 'react';
+// api
+import { login, register } from '../api/auth';
 
 const defaultAuthContext = {
   isAuthenticated: false, // 使用者是否登入的判斷依據，預設為 false，若取得後端的有效憑證，則切換為 true
-  currentMember: null,    // 當前使用者相關資料，預設為 null，成功登入後就會有使用者資料
-  register: null,         // 註冊方法
-  login: null,            // 登入方法
-  logout: null,           // 登入方法
+  currentMember: null, // 當前使用者相關資料，預設為 null，成功登入後就會有使用者資料
+  register: null, // 註冊方法
+  login: null, // 登入方法
+  logout: null, // 登入方法
 };
 
 const AuthContext = createContext(defaultAuthContext);
@@ -13,11 +16,25 @@ const AuthProvider = ({ children }) => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [payload, setPayload] = useState(null);
   return (
-    <AuthContext.Provider>
+    <AuthContext.Provider
       value={{
         isAuthenticated,
-        currentMember:payload,
-      }}
-    </AuthContext.Provider>
+        currentMember: payload,
+        register: async (data) => {
+          const { success } = await register({
+            account: data.account,
+            username: data.username,
+            password: data.password,
+            checkPassword: data.checkPassword,
+            email: data.email,
+          });
+          if (success) {
+            setIsAuthenticated(true);
+          } else {
+            setIsAuthenticated(false);
+          }
+          return success;
+        },
+      }}></AuthContext.Provider>
   );
 };
