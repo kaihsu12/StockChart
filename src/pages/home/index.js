f7ae98e8a2e898260900884fd3ecd55bf242427e
OwenLu <gg0125gg@gmail.com>
2023-07-15 17:30:20 +0800

    feat: add auth provider to home

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,27 +1,31 @@
 // react
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+// context
+import { AuthProvider } from 'src/contexts/AuthContext.jsx';
 // page
 import LoginPage from '../loginPage/loginPage';
 import SignupPage from '../signupPage/signupPage';
 import AdminLoginPage from '../adminLoginPage/adminLoginPage';
 import MainPage from '../mainPage/mainPage';
-import DiaryPage from '../diaryPage/diaryPage'
+import DiaryPage from '../diaryPage/diaryPage';
 // style
-import './reset.scss'
-import './base.scss'
+import './reset.scss';
+import './base.scss';
 
 const Home = () => {
   return (
     <div>
       <BrowserRouter>
-        <Routes>
-          <Route path='*' element={<LoginPage />} />
-          <Route path='signup' element={<SignupPage />} />
-          <Route path='admin' element={<AdminLoginPage />} />
-          <Route path='main' element={<MainPage />} />
-          <Route path='main/diary' element={<DiaryPage />} />
-        </Routes>
+        <AuthProvider>
+          <Routes>
+            <Route path='*' element={<LoginPage />} />
+            <Route path='signup' element={<SignupPage />} />
+            <Route path='admin' element={<AdminLoginPage />} />
+            <Route path='main' element={<MainPage />} />
+            <Route path='main/diary' element={<DiaryPage />} />
+          </Routes>
+        </AuthProvider>
       </BrowserRouter>
     </div>
   );
