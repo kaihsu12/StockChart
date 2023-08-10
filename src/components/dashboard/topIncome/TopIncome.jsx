743edb48938a0fbe23fe730f68ca7dd7df114017
kaihsu12 <kaihsu12@gmail.com>
2023-08-10 13:06:49 +0800

    change structure of html tag

--- src/components/dashboard/topIncome/TopIncome.jsx
+++ src/components/dashboard/topIncome/TopIncome.jsx
@@ -8,30 +8,37 @@ export const TopIncome = ({
   roundTrip, // 總趟次
   riskRatio, // 盈虧比
 }) => {
+  const netPAndLSum = netPAndL ? netPAndL : '0';
   return (
     <>
       <div className='topIncomeContainer'>
         <div className='toatalIncome'>
-          <div className='medium-14'>整體總損益</div>
-          <div className='bold-28 panl'>${netPAndL || 0}</div>
-          <div className='regular-12'>總交易量： {roundTrip}</div>
+          <span className='medium-14'>整體淨損益</span>
+          <span
+            className={`bold-28 panl ${
+              String(netPAndLSum).includes('-') ? 'red' : 'green'
+            } `}
+          >
+            {String(netPAndLSum).includes('-')
+              ? `$${netPAndLSum * 200}`
+              : `$+${netPAndLSum * 200}`}
+          </span>
+          <span className='regular-12'>總交易量： {roundTrip}</span>
         </div>
         <div className='averageIncome'>
-          <div className='average1'>
-            <div className='medium-14'>獲利係數</div>
+          <div className='average average1'>
+            <div className='medium-14'>盈虧比</div>
             <div className='bold-28 context1'>{riskRatio || 0}</div>
           </div>
-          <div className='average2'>
+          <div className='verticalLine'></div>
+          <div className='average average2'>
             <div className='medium-14'>平均獲利</div>
-            <div className='bold-28 context2'>
-              {averageWinPoints || '$0'}
-            </div>
+            <div className='bold-28 context2'>{averageWinPoints || '$0'}</div>
           </div>
-          <div className='average3'>
+          <div className='verticalLine'></div>
+          <div className='average average3'>
             <div className='medium-14'>平均虧損</div>
-            <div className='bold-28 context3'>
-              {averageLossPoints || '$0'}
-            </div>
+            <div className='bold-28 context3'>{averageLossPoints || '$0'}</div>
           </div>
         </div>
       </div>
