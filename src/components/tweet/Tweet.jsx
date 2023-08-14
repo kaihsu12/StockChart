b55cb5851632a19b23b908be45713c4576438e8b
kaihsu12 <kaihsu12@gmail.com>
2023-08-14 15:33:29 +0800

    render component according to ref available and implement API with useMutation

--- src/components/tweet/Tweet.jsx
+++ src/components/tweet/Tweet.jsx
@@ -1,6 +1,8 @@
 // hooks
+import React from 'react';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useMutation, useQueryClient } from 'react-query';
 // components
 import ReplyModal from '../replyModal/ReplyModal';
 import UnpublishBox from '../unpublishBox/UnpublishBox';
@@ -18,191 +20,311 @@ import optionIcon from '../../assets/options.svg';
 // style
 import './Tweet.scss';
 
-const Tweet = ({
-  tweetId,
-  name,
-  account,
-  tweetTime,
-  content,
-  action,
-  date,
-  quantity,
-  price,
-  likes,
-  isLiked,
-  replies,
-  reSetTweets,
-  activeTab,
-  avatar,
-}) => {
-  const [modal, setModal] = useState(false);
-  const [isVisible, setIsVisible] = useState(false);
-  const { checkItemId } = useId();
-  const navigate = useNavigate();
-  const dealTime = new Date(date);
+const Tweet = React.forwardRef(
+  (
+    {
+      tweetId,
+      name,
+      account,
+      tweetTime,
+      content,
+      action,
+      date,
+      quantity,
+      price,
+      likes,
+      isLiked,
+      replies,
+      activeTab,
+      avatar,
+    },
+    ref
+  ) => {
+    const [modal, setModal] = useState(false);
+    const [isVisible, setIsVisible] = useState(false);
+    const { checkItemId } = useId();
+    const navigate = useNavigate();
 
-  const handleId = () => {
-    checkItemId(tweetId);
-  };
+    const handleId = () => {
+      checkItemId(tweetId);
+    };
 
-  const handleLike = async () => {
-    try {
-      if (isLiked) {
-        await unlikeTweet(tweetId);
-        reSetTweets?.((prev) => {
-          return prev.map((tweet) => {
-            if (tweet.id === tweetId) {
-              return {
-                ...tweet,
-                like_count: tweet.like_count - 1,
-                is_liked: false,
-              };
-            }
-            return { ...tweet };
-          });
-        });
-      } else {
-        await likeTweet(tweetId);
-        reSetTweets?.((prev) => {
-          return prev.map((tweet) => {
-            if (tweet.id === tweetId) {
-              return {
-                ...tweet,
-                like_count: Number(tweet.like_count) + 1,
-                is_liked: true,
-              };
-            }
-            return { ...tweet };
-          });
-        });
+    // useMutation - start //
+    const queryClient = useQueryClient();
+    const unLikePostMutation = useMutation({
+      mutationFn: async (id) => {
+        return await unlikeTweet(id);
+      },
+      onSuccess: () => {
+        queryClient.invalidateQueries('/posts');
+      },
+    });
+
+    const likePostMutation = useMutation({
+      mutationFn: async (id) => {
+        return await likeTweet(id);
+      },
+      onSuccess: () => {
+        queryClient.invalidateQueries('/posts');
+      },
+    });
+
+    // useMutation - end //
+
+    const handleLike = async () => {
+      try {
+        if (isLiked) {
+          unLikePostMutation.mutate(tweetId);
+        } else {
+          likePostMutation.mutate(tweetId);
+        }
+      } catch (error) {
+        console.error('liking failed:', error);
       }
-    } catch (error) {
-      console.error('liking failed:', error);
-    }
-  };
+    };
 
-  const toggleModal = () => {
-    setModal(!modal);
-  };
+    const toggleModal = () => {
+      setModal(!modal);
+    };
 
-  return (
-    <>
-      <div
-        className='tweet'
-        onClick={(e) => {
-          if (
-            e.target.className !== 'like' &&
-            e.target.className !== 'comment' &&
-            e.target.className !== 'option' &&
-            e.target.className !== 'optionIcon' &&
-            e.target.innerText !== '隱藏公開'
-          ) {
-            checkItemId(tweetId);
-            navigate('/reply');
-          } else if (e.target.innerText !== '隱藏公開' && isVisible === true) {
-            setIsVisible(!isVisible);
-          }
-        }}
-      >
-        <div className='tweetMain'>
-          <div className='tweetTop'>
-            <div className='userWidget'>
-              <img className='userImg' src={avatar} alt='user-img' />
-              <span className='userInfo'>
-                <p className='bold-14'>{`${
-                  name.slice(0, 1).toUpperCase() + name.slice(1)
-                }`}</p>
-                <p className='regular-14'>@{account}</p>
-              </span>
+    return (
+      <>
+        {ref ? (
+          <div
+            ref={ref}
+            className='tweet'
+            onClick={(e) => {
+              if (
+                e.target.className !== 'like' &&
+                e.target.className !== 'comment' &&
+                e.target.className !== 'option' &&
+                e.target.className !== 'optionIcon' &&
+                e.target.innerText !== '隱藏公開'
+              ) {
+                checkItemId(tweetId);
+                navigate('/reply');
+              } else if (
+                e.target.innerText !== '隱藏公開' &&
+                isVisible === true
+              ) {
+                setIsVisible(!isVisible);
+              }
+            }}
+          >
+            <div className='tweetMain'>
+              <div className='tweetTop'>
+                <div className='userWidget'>
+                  <img className='userImg' src={avatar} alt='user-img' />
+                  <span className='userInfo'>
+                    <p className='bold-14'>{`${
+                      name.slice(0, 1).toUpperCase() + name.slice(1)
+                    }`}</p>
+                    <p className='regular-14'>@{account}</p>
+                  </span>
+                </div>
+                <span className='date regular-14'>{tweetTime}</span>
+                {activeTab === 'user' && (
+                  <span
+                    className='option'
+                    onClick={() => setIsVisible(!isVisible)}
+                  >
+                    <img
+                      className='optionIcon'
+                      src={optionIcon}
+                      alt='option-icon'
+                    />
+                  </span>
+                )}
+              </div>
+
+              <div className='tweetMsg'>
+                <p className='regular-16'>{content}</p>
+              </div>
+
+              <div className='singleTrade'>
+                <ul className='formTitles bold-14'>
+                  <li>類型</li>
+                  <li>日期</li>
+                  <li>交易量</li>
+                  <li>成交價</li>
+                </ul>
+                <ul className='formCotent regular-14'>
+                  <li>
+                    <span>
+                      <img
+                        src={action === 'buy' ? buyIcon : sellIcon}
+                        alt='trade-Icon'
+                      />
+                    </span>
+                    {action === 'buy' ? (
+                      <p className='buy'>買</p>
+                    ) : (
+                      <p className='sell'>賣</p>
+                    )}
+                  </li>
+                  <li>
+                    <p>{date.substr(0, 10)}</p>
+                    <p>{date.substr(11, 8)}</p>
+                  </li>
+                  <li>x{quantity}</li>
+                  <li className={action === 'buy' ? 'buyPrice' : 'sellPrice'}>
+                    ${price}
+                  </li>
+                </ul>
+              </div>
             </div>
-            <span className='date regular-14'>{tweetTime}</span>
-            {activeTab === 'user' && (
-              <span className='option' onClick={() => setIsVisible(!isVisible)}>
-                <img
-                  className='optionIcon'
-                  src={optionIcon}
-                  alt='option-icon'
-                />
-              </span>
+
+            {activeTab === 'all' && (
+              <div className='tweetControl'>
+                <span className='likes'>
+                  <img
+                    className='like'
+                    src={isLiked ? heartFilled : heartHollow}
+                    alt='heart-Icon'
+                    onClick={handleLike}
+                  />
+                  <p className='medium-14'>{likes}</p>
+                </span>
+                <span className='verticalLine'></span>
+                <span className='comments' onClick={handleId}>
+                  <img
+                    className='comment'
+                    src={commentIcon}
+                    alt='comment-Icon'
+                    onClick={toggleModal}
+                  />
+                  <p className='medium-14'>{replies}</p>
+                </span>
+              </div>
+            )}
+            {isVisible && activeTab === 'user' && (
+              <UnpublishBox
+                setVisible={setIsVisible}
+                visible={isVisible}
+                tweetId={tweetId}
+              />
             )}
           </div>
-
-          <div className='tweetMsg'>
-            <p className='regular-16'>{content}</p>
-          </div>
-
-          <div className='singleTrade'>
-            <ul className='formTitles bold-14'>
-              <li>類型</li>
-              <li>日期</li>
-              <li>交易量</li>
-              <li>成交價</li>
-            </ul>
-            <ul className='formCotent regular-14'>
-              <li>
-                <span>
-                  <img
-                    src={action === 'buy' ? buyIcon : sellIcon}
-                    alt='trade-Icon'
-                  />
-                </span>
-                {action === 'buy' ? (
-                  <p className='buy'>買</p>
-                ) : (
-                  <p className='sell'>賣</p>
+        ) : (
+          <div
+            className='tweet'
+            onClick={(e) => {
+              if (
+                e.target.className !== 'like' &&
+                e.target.className !== 'comment' &&
+                e.target.className !== 'option' &&
+                e.target.className !== 'optionIcon' &&
+                e.target.innerText !== '隱藏公開'
+              ) {
+                checkItemId(tweetId);
+                navigate('/reply');
+              } else if (
+                e.target.innerText !== '隱藏公開' &&
+                isVisible === true
+              ) {
+                setIsVisible(!isVisible);
+              }
+            }}
+          >
+            <div className='tweetMain'>
+              <div className='tweetTop'>
+                <div className='userWidget'>
+                  <img className='userImg' src={avatar} alt='user-img' />
+                  <span className='userInfo'>
+                    <p className='bold-14'>{`${
+                      name.slice(0, 1).toUpperCase() + name.slice(1)
+                    }`}</p>
+                    <p className='regular-14'>@{account}</p>
+                  </span>
+                </div>
+                <span className='date regular-14'>{tweetTime}</span>
+                {activeTab === 'user' && (
+                  <span
+                    className='option'
+                    onClick={() => setIsVisible(!isVisible)}
+                  >
+                    <img
+                      className='optionIcon'
+                      src={optionIcon}
+                      alt='option-icon'
+                    />
+                  </span>
                 )}
-              </li>
-              <li>
-                <p>{date.substr(0, 10)}</p>
-                <p>{date.substr(11, 8)}</p>
-              </li>
-              <li>x{quantity}</li>
-              <li className={action === 'buy' ? 'buyPrice' : 'sellPrice'}>
-                ${price}
-              </li>
-            </ul>
-          </div>
-        </div>
+              </div>
 
-        {activeTab === 'all' && (
-          <div className='tweetControl'>
-            <span className='likes'>
-              <img
-                className='like'
-                src={isLiked ? heartFilled : heartHollow}
-                alt='heart-Icon'
-                onClick={handleLike}
+              <div className='tweetMsg'>
+                <p className='regular-16'>{content}</p>
+              </div>
+
+              <div className='singleTrade'>
+                <ul className='formTitles bold-14'>
+                  <li>類型</li>
+                  <li>日期</li>
+                  <li>交易量</li>
+                  <li>成交價</li>
+                </ul>
+                <ul className='formCotent regular-14'>
+                  <li>
+                    <span>
+                      <img
+                        src={action === 'buy' ? buyIcon : sellIcon}
+                        alt='trade-Icon'
+                      />
+                    </span>
+                    {action === 'buy' ? (
+                      <p className='buy'>買</p>
+                    ) : (
+                      <p className='sell'>賣</p>
+                    )}
+                  </li>
+                  <li>
+                    <p>{date.substr(0, 10)}</p>
+                    <p>{date.substr(11, 8)}</p>
+                  </li>
+                  <li>x{quantity}</li>
+                  <li className={action === 'buy' ? 'buyPrice' : 'sellPrice'}>
+                    ${price}
+                  </li>
+                </ul>
+              </div>
+            </div>
+
+            {activeTab === 'all' && (
+              <div className='tweetControl'>
+                <span className='likes'>
+                  <img
+                    className='like'
+                    src={isLiked ? heartFilled : heartHollow}
+                    alt='heart-Icon'
+                    onClick={handleLike}
+                  />
+                  <p className='medium-14'>{likes}</p>
+                </span>
+                <span className='verticalLine'></span>
+                <span className='comments' onClick={handleId}>
+                  <img
+                    className='comment'
+                    src={commentIcon}
+                    alt='comment-Icon'
+                    onClick={toggleModal}
+                  />
+                  <p className='medium-14'>{replies}</p>
+                </span>
+              </div>
+            )}
+            {isVisible && activeTab === 'user' && (
+              <UnpublishBox
+                setVisible={setIsVisible}
+                visible={isVisible}
+                tweetId={tweetId}
               />
-              <p className='medium-14'>{likes}</p>
-            </span>
-            <span className='verticalLine'></span>
-            <span className='comments' onClick={handleId}>
-              <img
-                className='comment'
-                src={commentIcon}
-                alt='comment-Icon'
-                onClick={toggleModal}
-              />
-              <p className='medium-14'>{replies}</p>
-            </span>
+            )}
           </div>
         )}
-        {isVisible && activeTab === 'user' && (
-          <UnpublishBox
-            setVisible={setIsVisible}
-            visible={isVisible}
-            tweetId={tweetId}
-            reSetTweets={reSetTweets}
-          />
-        )}
-      </div>
-      <ReplyModal
-        isActivated={modal}
-        toggle={toggleModal}
-        reSetTweets={reSetTweets}
-      />
-    </>
-  );
-};
+        <ReplyModal isActivated={modal} toggle={toggleModal} />
+      </>
+    );
+  }
+);
 
 export default Tweet;
