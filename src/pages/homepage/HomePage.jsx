0aaba063e3a55c3cc7d1c55f132e27386e2cf881
kaihsu12 <kaihsu12@gmail.com>
2023-08-29 20:35:57 +0800

    amend homepage

--- src/pages/homepage/HomePage.jsx
+++ src/pages/homepage/HomePage.jsx
@@ -1,6 +1,8 @@
 import { useAuth } from '../../contexts/AuthContext';
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import googleLogo from '../../assets/google-icon.svg';
+import { gooleLogin } from '../../api/auth';
 
 const HomePage = () => {
   const { isAuthenticated } = useAuth();
@@ -14,9 +16,24 @@ const HomePage = () => {
   //   }
   // }, [navigate, isAuthenticated]);
 
+  const handleGoogleLogin = async () => {
+    try {
+      await gooleLogin();
+    } catch (error) {
+      console.error('[Login Failed]:', error);
+    }
+  };
+
   return (
     <div>
       <p>homepage</p>
+      <button
+        className='googleBtn secondary-button'
+        onClick={handleGoogleLogin}
+      >
+        <img src={googleLogo} alt='google-logo' />
+        <span className='bold-18'>Google登入</span>
+      </button>
     </div>
   );
 };
