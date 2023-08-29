9f51836348cb3715a9d3d31a64ab19c543e6c51c
kaihsu12 <kaihsu12@gmail.com>
2023-08-29 18:07:43 +0800

    new route

--- src/pages/homepage/HomePage.jsx
+++ src/pages/homepage/HomePage.jsx
@@ -10,7 +10,7 @@ const HomePage = () => {
     if (isAuthenticated) {
       navigate('/main');
     } else {
-      navigate('/login');
+      navigate('/authtest');
     }
   }, [navigate, isAuthenticated]);
 };
