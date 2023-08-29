ba00d2efc1a3e2def72bb06e113d82ab589d8a4a
kaihsu12 <kaihsu12@gmail.com>
2023-08-29 20:46:44 +0800

    return to the usual

--- src/pages/homepage/HomePage.jsx
+++ src/pages/homepage/HomePage.jsx
@@ -1,41 +1,18 @@
 import { useAuth } from '../../contexts/AuthContext';
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import googleLogo from '../../assets/google-icon.svg';
-import { gooleLogin } from '../../api/auth';
 
 const HomePage = () => {
   const { isAuthenticated } = useAuth();
   const navigate = useNavigate();
 
-  // useEffect(() => {
-  //   if (isAuthenticated) {
-  //     navigate('/main');
-  //   } else {
-  //     navigate('/login');
-  //   }
-  // }, [navigate, isAuthenticated]);
-
-  const handleGoogleLogin = async () => {
-    try {
-      await gooleLogin();
-    } catch (error) {
-      console.error('[Login Failed]:', error);
+  useEffect(() => {
+    if (isAuthenticated) {
+      navigate('/main');
+    } else {
+      navigate('/login');
     }
-  };
-
-  return (
-    <div>
-      <p>homepage</p>
-      <button
-        className='googleBtn secondary-button'
-        onClick={handleGoogleLogin}
-      >
-        <img src={googleLogo} alt='google-logo' />
-        <span className='bold-18'>Google登入</span>
-      </button>
-    </div>
-  );
+  }, [navigate, isAuthenticated]);
 };
 
 export default HomePage;
